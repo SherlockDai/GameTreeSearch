f5475b07116f58bca11a0d34213c13a93b1e59de
SherlockDai <daiyue@usc.edu>
2017-06-20 19:32:32 -0700

    structure and possible-function
    add minimax function
    add minValue function
    add maxValue function
    add a part of find possible move function

--- hw2cs561s17.py
+++ hw2cs561s17.py
@@ -18,8 +18,21 @@ def readInput():
     colors = sortByAlphabet(colors)
     playersAndFirstMove = lines[1].split(',')
     maxDepthOfTree = lines[2]
-    player1Scores = lines[3]
-    player2Scores = lines[4]
+    player1ScoresList = lines[3]
+    player1ScoresList = player1ScoresList.split(",")
+    player1Scores = {}
+    for score in player1ScoresList:
+        player1ScoreInfo = score.split(":")
+        player1Scores[player1ScoreInfo[0]] = player1ScoreInfo[1]
+    player2ScoresList = lines[4]
+    player2ScoresList = player2ScoresList.split(",")
+    player2Scores = {}
+    for score in player2ScoresList:
+        player2ScoreInfo = score.split(":")
+        player2Scores[player2ScoreInfo[0]] = player2ScoreInfo[1]
+    scores = {}
+    scores['1'] = player1Scores
+    scores['2'] = player2Scores
     connections = {}
     for line in lines[5:]:
         # establish the connection between cities
@@ -30,7 +43,7 @@ def readInput():
         connections[city] = neighbors
 
     return {'colors': colors, 'playersAndFirstMove': playersAndFirstMove, 'maxDepthOfTree': maxDepthOfTree,
-            'player1Scores': player1Scores, 'player2Scores': player2Scores, 'connections': connections}
+            'scores': scores, 'connections': connections}
 
 # define the function to sort list based on alphabet
 def sortByAlphabet(list):
@@ -54,18 +67,91 @@ def sortByAlphabet(list):
 
 # define class state which store the state of game
 class state:
+    depth = 0
     parent = None
     alpha = float("-inf")
     beta = float("inf")
-    v = float("-inf")
+    nextMove = []
     toBeExplored = []
     assignment = {}
+    playersScore = {'1': 0, '2': 0}
+    playersCities = {'1': [], '2': []}
     def __init__(self, connections):
         for city in connections:
             self.toBeExplored.append(city)
-            self.assignment[city] = []
+            self.assignment[city] = ""
         self.toBeExplored = sortByAlphabet(self.toBeExplored)
+
+# define minimax function here
+def minimax(state, input):
+    return maxValue(state, input)
+# define the function to find all possible next moves and check depth
+def terminalTest(state, input):
+    # check the depth
+    if state.depth == input['maxDepthOfTree']:
+        return []
+    else:
+        for city in state.toBeExplored:
+            for color in input['colors']:
+                neighbors = input['connecitons'][city]
+                for neighbor in neighbors:
+                    if state.assignment[neighbor] == color:
+                        #skip this color and checck the next one
+                        break
+                if state.assignment[neighbor] == color:
+                    continue
+                else:
+                    
+
+# define maxValue fuction here for player1
+def maxValue(state, input):
+    moves = terminalTest(state, input)
+    if moves.length == 0:
+        return state
+    else:
+        v = float('-inf')
+        for move in moves:
+            v = max(v, minValue(move))
+            if v >= state.beta:
+                return state
+            else:
+                state.alpha = max(state.alpha, v)
+        return state
+
+# define minValue function here for player2
+def minValue(state, input):
+    moves = terminalTest(state, input)
+    if moves.length == 0:
+        return state
+    else:
+        v = float('inf')
+        for move in moves:
+            state.v = min(v, maxValue(move))
+            if v <= state.alpha:
+                return state
+            else:
+                state.beta = min(state.beta, v)
+        return state
+
 # main()
 input = readInput()
 rootNode = state(input['connections'])
-test = 0;
+#initiate the information of the rootNode
+for firstMove in input["playersAndFirstMove"]:
+    firstMoveInfo = firstMove.split("-")
+    #store the player number
+    player = firstMoveInfo[1]
+    #extract city name and color assigned
+    firstMoveInfo = firstMoveInfo[0].split(":")
+    city = firstMoveInfo[0]
+    color = firstMoveInfo[1]
+    if city in rootNode.toBeExplored:
+        rootNode.toBeExplored.remove(city)
+    #extract the score of the color based on the current player
+    score = input['scores'][player][color]
+    #update assignment in rootNode state
+    rootNode.assignment[city] = color
+    rootNode.playersCities[player].append(city)
+    rootNode.playersScore[player] = rootNode.playersScore[player] + int(score)
+
+    test = 0;
